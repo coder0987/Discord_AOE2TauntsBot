7b80471a8975b44da8cfa4bee4b77118d334a3da
server <webmaster@smach.us>
2023-10-25 20:01:13 -0500

    Fixes some practical server issues

--- commands/help/list.js
+++ commands/help/list.js
@@ -10,16 +10,20 @@ module.exports = {
         		.setDescription('Find a taunt that contains the specified text')),
 	async execute(interaction) {
 		let msg = '';
-        let textToFind = interaction.data.options[0].value;
-        if (textToFind) {
-            textToFind = textToFind.toLowerCase();
-        } else {
-            textToFind = '';
+        let textToFind = '';
+
+        if (interaction.data && interaction.data.options && interaction.data.options[0]) {
+            textToFind = interaction.data.options[0].value.toLowerCase();
         }
+
         for (let i in TAUNTS_TO_TEXT) {
             if (TAUNTS_TO_TEXT[i].toLowerCase().includes(textToFind)) {
-                msg += i + '.' + TAUNTS_TO_TEXT[i] + '\n';
+                msg += i + '. ' + TAUNTS_TO_TEXT[i] + '\n';
             }
+        }
+
+        if (msg.length >= 2000) {
+            msg = msg.substring(0,1950) + '...';
         }
 
 		await interaction.reply({ content: msg, ephemeral: true});
